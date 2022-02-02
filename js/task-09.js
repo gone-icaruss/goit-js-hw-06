797db8df0cd88b5873aef0b1372cee9e7b42ee1b
Omars Alifhanovs <78239462+gone-icaruss@users.noreply.github.com>
2022-02-02 20:22:23 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,15 +1,19 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
 
 const btnColor = document.querySelector('.change-color');
 const span = document.querySelector('.color');
 
 btnColor.addEventListener('click',changeBackGroundColor);
 
-function changeBackGroundColor(evt) {
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+function changeBackGroundColor() {
   const selectedColor = getRandomHexColor()
   span.textContent = `${selectedColor}`;
-  span.style.color = selectedColor;
   document.body.style.background = selectedColor
 }
+
+
