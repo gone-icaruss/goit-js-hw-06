6584c24330489745720a8d94370ff86794dcaf37
Omars Alifhanovs <78239462+gone-icaruss@users.noreply.github.com>
2022-02-05 20:49:09 +0200

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -13,9 +13,9 @@
 
 
    
-const categoriesEl = document.querySelectorAll(`.item`);
+const categoryRef = document.querySelectorAll(`.item`);
 
-categoriesEl.forEach(category => {
+categoryRef.forEach(category => {
     console.log('Категория:', category.querySelector('h2').textContent);
     console.log('Категория:', category.querySelectorAll('li').length)
 })
